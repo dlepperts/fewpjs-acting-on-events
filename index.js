a111af5a99b4aea3ba71700d57ddd5c98de38053
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-02-02 09:32:20 -0600

    changes

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ function moveDodgerLeft() {
     if (e.key === "ArrowLeft") {
         moveDodgerLeft();
     }
+  }); 
+
+  document.addEventListener("keydown", function(e) {
     if (e.key === "ArrowRight") {
         moveDodgerRight();
     }
